dfdcc088d2f46134046aaa0d34757d430106d16e
Tom Shea <tom@shea.at>
2013-02-20 01:06:58 +0000

    Very basic start on player tests

--- tests/player.rb
+++ tests/player.rb
@@ -1,3 +1,14 @@
 describe Player do
+  before do
+    @player = Player.new
+  end
+
+  it "can be created with no arguments" do
+    @player.must_be_instance_of Player
+  end
+
+  it "can be given a card" do
+
+  end
   
 end
