57e05a7df36d06058c566206d50cd0105a85ca7d
Colin Shea <colin@evaryont.me>
2013-03-14 19:15:07 -0700

    The library can be cleared
    Oh, and also change all the describe blocks to it blocks. Stupid me.

--- test/test_library.rb
+++ test/test_library.rb
@@ -3,7 +3,7 @@ require 'fluxx/library'
 require 'fluxx/card'
 
 describe Fluxx::Library do
-  describe "can fetch a card given it's name" do
+  it "can fetch a card given it's name" do
     # Card.create factory appends the card to the Library
     shackles = Fluxx::Card.create(type: :creeper, name: "Shackles", description: "You can't win if you have this unless the Goal says otherwise.")
     # Retrieve a card from the library
@@ -12,10 +12,17 @@ describe Fluxx::Library do
     other_shackles.must_be_same_as shackles
   end
 
-  describe "can't create the same card twice" do
+  it "can't create the same card twice" do
     Fluxx::Card.create(type: :creeper, name: "Shackles", description: "You can't win if you have this unless the Goal says otherwise.")
     proc {
       Fluxx::Card.create(type: :creeper, name: "Shackles", description: "Matches on the card name, ignored!")
     }.must_raise Fluxx::CardAlreadyExistsError
+  end
+
+  it "can be cleared" do
+    Fluxx::Card.create(type: :creeper, name: "Shackles", description: "You can't win if you have this unless the Goal says otherwise.")
+    Fluxx::Library.clear!
+    # No exceptions throw
+    Fluxx::Card.create(type: :creeper, name: "Shackles", description: "Matches on the card name, ignored!")
   end
 end
