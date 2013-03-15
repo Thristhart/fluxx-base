753d4cc46c6046cbecc2671454e096484efeb2f2
Tom Shea <tom@shea.at>
2013-03-15 03:46:58 +0000

    Basic Library function
    Can append cards, will throw error when card already exists, can access
    cards

--- lib/fluxx/library.rb
+++ lib/fluxx/library.rb
@@ -1,5 +1,12 @@
 require 'fluxx'
 class Fluxx::Library
+  @@cards = {}
+  def self.Append(card)
+    raise CardAlreadyExistsError if @@cards.include? card.name
+  end
 
+  def self.[](name)
+    return @@cards[name]
+  end
 
 end
