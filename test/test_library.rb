ae59d7a0d099b8a5127d72cf38ad1f8cadebd8ae
Colin Shea <colin@evaryont.me>
2013-03-14 20:58:28 -0700

    We have to save the library state
    Fluxx::Library is a global state, which means any changes, like clearing it,
    will cause other tests to go wonky. Save the state between each of the tests for
    the Library itself.

--- test/test_library.rb
+++ test/test_library.rb
@@ -4,7 +4,14 @@ require 'fluxx/card'
 
 describe Fluxx::Library do
   before do
+    # Save the internal library hash before clearing it
+    @saved_library = Fluxx::Library.instance.dup
     Fluxx::Library.clear!
+  end
+
+  after do
+    # And set it again later
+    Fluxx::Library.instance = @saved_library
   end
 
   it "can fetch a card given it's name" do
@@ -26,7 +33,7 @@ describe Fluxx::Library do
   it "can be cleared" do
     Fluxx::Card.create(type: :creeper, name: "Shackles", description: "You can't win if you have this unless the Goal says otherwise.")
     Fluxx::Library.clear!
-    # No exceptions throw
-    Fluxx::Card.create(type: :creeper, name: "Shackles", description: "Matches on the card name, ignored!")
+    # No exceptions thrown
+    Fluxx::Card.create(type: :creeper, name: "Shackles", description: "You can't win if you have this unless the Goal says otherwise.")
   end
 end
