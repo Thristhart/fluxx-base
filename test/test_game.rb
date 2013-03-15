4695acb22a1b9d3efc7dd547a64923d4be77b6ba
Colin Shea <colin@evaryont.me>
2013-03-14 18:29:01 -0700

    Basic starting conditions for a game: A deck & 2 players
    A game must start with at least the minimum:
    - 2, or more, players
    - A deck of cards

--- test/test_game.rb
+++ test/test_game.rb
@@ -8,8 +8,26 @@ describe Fluxx::Game do
     @game = Fluxx::Game.new
   end
 
-  describe "starts empty" do
+  it "starts empty" do
     @game.players.must_be_empty
     @game.deck.must_be_empty
+  end
+
+  it "must have at least 2 players before starting or error" do
+    proc { @game.start }.must_raise Fluxx::NotEnoughPlayersError
+  end
+
+  it "must have a deck before starting or error" do
+    @game.players << Fluxx::Player.new
+    @game.players << Fluxx::Player.new
+    proc { @game.start }.must_raise Fluxx::MissingDeckError
+  end
+
+  it "must have at least 2 players and a deck" do
+    @game.players << Fluxx::Player.new
+    @game.players << Fluxx::Player.new
+    @game.deck = Fluxx::Library.set(:pirate)
+
+    @game.start
   end
 end
