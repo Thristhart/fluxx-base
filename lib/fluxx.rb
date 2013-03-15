5b35ca91f940acfb42cc8593920c880f94a82f2d
Tom Shea <tom@shea.at>
2013-03-15 03:46:50 +0000

    Removed unused deck require, added library require

--- lib/fluxx.rb
+++ lib/fluxx.rb
@@ -1,11 +1,12 @@
 module Fluxx
+  require 'fluxx/library'
   require 'fluxx/version'
   require 'fluxx/game'
-  require 'fluxx/deck'
   require 'fluxx/player'
   require 'fluxx/card'
 
   class Error < StandardError; end
 
   class MissingAttributeError < Error; end
+  class CardAlreadyExistsError < Error; end
 end
