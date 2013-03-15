676c9f53e7b80b6b445488c2f85330ea50bb5e8d
Tom Shea <tom@shea.at>
2013-03-15 02:25:56 +0000

    Basic card implementation
    Fluxx::Card.create and Card.new. Only stores data for now

--- lib/fluxx/card.rb
+++ lib/fluxx/card.rb
@@ -1,4 +1,17 @@
 module Fluxx
   class Card
+    def self.create(options, play_behavior, cleanup_behavior)
+      type = options.delete :type
+      const_get(type.to_s.capitalize).new(options, play_behavior, cleanup_behavior)
+    end
+
+    def initialize(options, play_behavior, cleanup_behavior)
+      @play_behavior = play_behavior
+      @cleanup_behavior = cleanup_behavior
+
+      @name = options[:name]
+      @description = options[:description]
+      @set = options[:set]
+    end
   end
 end
