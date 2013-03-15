45b55c821d42d4d46fd47980c038ea467c1ec2c7
Colin Shea <colin@evaryont.me>
2013-03-14 18:49:53 -0700

    Expanding the tests on Player a bit
    Tests 2 basic functions right now:
    - A player can be given a card
    - If the card is a creeper, don't put it into the player's hand, but instead
    immediately add it to the player's creeper collection.

--- test/test_player.rb
+++ test/test_player.rb
@@ -4,13 +4,24 @@ require 'fluxx/player'
 describe Fluxx::Player do
   before do
     @player = Fluxx::Player.new
+    @shackles = Fluxx::Card.create(type: :creeper, name: "Shackles", description: "You can't win if you have this unless the Goal says otherwise.")
+    @sloop = Fluxx::Card.create(type: :keeper, name: "Sloop", category: :ship, set: 'Pirate')
   end
 
   it "can be created with no arguments" do
-    @player.must_be_instance_of Fluxx::Player
-    @player.hand.must_equal []
-    @player.keepers.must_equal []
-    @player.creepers.must_equal []
-    @player.titles.must_equal []
+    @player.hand.must_be_empty
+    @player.keepers.must_be_empty
+    @player.creepers.must_be_empty
+  end
+
+  it "can be given a card" do
+    @player.give @sloop
+    @player.hand.size.must_equal 1
+  end
+
+  it "given a creeper, immediately be played" do
+    @player.give @creeper
+    @player.hand.size.must_equal 0
+    @player.creepers.size.must_equal 0
   end
 end
