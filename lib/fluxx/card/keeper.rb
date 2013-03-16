10c2aa34ca8fa880a79aa9921b05914748078aef
Tom Shea <tom@shea.at>
2013-03-16 01:17:28 +0000

    Keepers can have a category

--- lib/fluxx/card/keeper.rb
+++ lib/fluxx/card/keeper.rb
@@ -1,6 +1,11 @@
 require 'fluxx'
 require 'fluxx/card'
 class Fluxx::Card::Keeper < Fluxx::Card
+  attr_accessor :category
+  def initialize(options, play_behavior, cleanup_behavior)
+    super
+    @category = options[:category]
+  end
   def play(ruleset, player)
     super
     player.keepers << self
