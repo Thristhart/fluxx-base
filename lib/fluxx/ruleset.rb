8440703867587ed09a16f94512567510820be8a5
Tom Shea <tom@shea.at>
2013-03-19 19:27:47 +0000

    Start on implementing a ruleset

--- lib/fluxx/ruleset.rb
+++ lib/fluxx/ruleset.rb
@@ -1,4 +1,17 @@
 require 'fluxx'
 class Fluxx::Ruleset
+  attr_accessor :play_limit, :draw_limit, :goal_limit, :keeper_limit
+  def initialize
+    @defaults = {
+      play_limit: 1,
+      draw_limit: 1,
+      goal_limit: 1,
+      keeper_limit: 1}
+    @play_limit = @draw_limit = 1
+  end
+
+  def default(variable)
+    
+  end
 
 end
