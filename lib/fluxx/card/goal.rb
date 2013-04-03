f158aa7d7d75298aa79ab0f28be1b6b0dafe0193
Tom Shea <tom@shea.at>
2013-04-03 18:35:38 +0000

    Added validation to goals

--- lib/fluxx/card/goal.rb
+++ lib/fluxx/card/goal.rb
@@ -3,15 +3,31 @@ require 'fluxx/card'
 class Fluxx::Card::Goal < Fluxx::Card
   attr_reader :goal, :categories
   def initialize(options)
+    super
+
     raise Fluxx::MissingAttributeError unless options[:goal]
 
-    known_types = [:cards, :needs, :either]
+    @known_types = [:cards, :needs, :either]
     # make sure at least one of the known types is in here
-    raise Fluxx::MissingAttributeError unless (options[:goal].keys & known_types).any?
+    raise Fluxx::MissingAttributeError unless (options[:goal].keys & @known_types).any?
 
 
     @goal = options[:goal]
-    @categories = @goal - known_types
+    @categories = @goal.keys - @known_types
+
+    validate
+
+  end
+
+  def validate
+    @known_types.each do |type|
+      if(@goal[type])
+       raise Fluxx::MissingAttributeError unless @goal[type].any? 
+       @goal[type].each do |requirement|
+         raise Fluxx::UnobtainableGoalError Fluxx::Library[requirement]
+       end
+      end
+    end
   end
 
 end
