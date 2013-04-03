343bd1ab776dce467bb9e464419b83d3af2d3516
Tom Shea <tom@shea.at>
2013-04-03 18:30:11 +0000

    Implemented variable defaulting

--- lib/fluxx/ruleset.rb
+++ lib/fluxx/ruleset.rb
@@ -1,17 +1,19 @@
 require 'fluxx'
 class Fluxx::Ruleset
-  attr_accessor :play_limit, :draw_limit, :goal_limit, :keeper_limit
+  attr_accessor :play_limit, :draw_limit, :goal_limit, :keeper_limit, :criteria
   def initialize
     @defaults = {
       play_limit: 1,
       draw_limit: 1,
       goal_limit: 1,
       keeper_limit: 1}
-    @play_limit = @draw_limit = 1
+
+    @defaults.keys.each do |k|
+      default(k)
+    end
   end
 
   def default(variable)
-    
+    instance_variable_set('@' + variable.to_s, @defaults[variable])
   end
-
 end
