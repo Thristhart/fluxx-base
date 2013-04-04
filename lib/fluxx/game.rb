618436e854552a41cecc7ac407b69797e2837f1a
Tom Shea <tom@shea.at>
2013-04-04 20:23:51 +0000

    Basic start exceptions

--- lib/fluxx/game.rb
+++ lib/fluxx/game.rb
@@ -7,8 +7,17 @@ class Fluxx::Game
   end
 
   def reset!
+    @started = false
     @deck = []
     @players = []
     @ruleset = Fluxx::Ruleset.new()
+  end
+
+  def start
+    raise Fluxx::GameAlreadyStartedError if @started
+    raise Fluxx::NotEnoughPlayersError if @players.length < 2  
+    raise Fluxx::MissingDeckError if @deck.length == 0
+
+    @started = true
   end
 end
