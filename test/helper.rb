7bf266f25b3880c1d76885111ebc481dc69517bf
Tom Shea <tom@shea.at>
2013-04-04 20:23:26 +0000

    Describe the set as a symbol rather than a string

--- test/helper.rb
+++ test/helper.rb
@@ -12,7 +12,7 @@ Fluxx::Library.clear!
 
 # Pretty basic keepers, that has a category. NB: These need to be defined before
 # the goals, to make sure the criteria solving code succeeds.
-Fluxx::Card.create(type: :keeper, name: 'Sloop', set: "Pirate", category: :ship)
+Fluxx::Card.create(type: :keeper, name: 'Sloop', set: :pirate, category: :ship)
 Fluxx::Card.create(type: :keeper, name: 'Strongbox')
 Fluxx::Card.create(type: :keeper, name: 'Flintlock Pistol')
 Fluxx::Card.create(type: :keeper, name: 'Cannon')
@@ -27,28 +27,28 @@ Fluxx::Card.create(type: :keeper, name: 'Emeralds', catgeory: :booty)
 # An example card that includes a category requirement
 Fluxx::Card.create(type: :goal,
                    name: "Treasure Chest",
-                   set:  'Pirate',
+                   set:  :pirate,
                    goal: { cards:["Strongbox"], booty:2 })
 
 # The only goal in the entire Pirate set that contains an 'or'
 Fluxx::Card.create(type: :goal,
                    name: "Munitions",
-                   set:  'Pirate',
+                   set:  :pirate,
                    goal: { either: ["Flintlock Pistol", "Cannon"], cards:["Gunpowder"] })
 
 # A fairly basic goal
 Fluxx::Card.create(type: :goal,
                    name: "Key Lime Pie",
-                   set:  'Pirate',
+                   set:  :pirate,
                    goal: { cards: ["Key", "Limes"] })
 
 # This one requires a subset of cards
 Fluxx::Card.create(type: :goal,
                    name: "Gems",
-                   set:  'Pirate',
+                   set:  :pirate,
                    goal: { needs: [3, "Rubies", "Pearls", "Diamonds", "Emeralds"] })
 
-Fluxx::Card.create(type: :creeper, name: "Shackles", description: "You can't win if you have this unless the Goal says otherwise.", set: "Pirate")
+Fluxx::Card.create(type: :creeper, name: "Shackles", description: "You can't win if you have this unless the Goal says otherwise.", set: :pirate)
 
 Fluxx::Card.create(type: :rule, name: 'Play 4', description: 'Play 4 cards', mod: :play_limit, new: 4)
 Fluxx::Card.create(type: :rule, name: 'Play 2', description: 'Play 2 cards', mod: :play_limit, new: 2)
