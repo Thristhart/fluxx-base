0e9538553f08b15ef9d1dce86aebe83f00643b9f
Colin Shea <colin@evaryont.me>
2013-04-08 10:31:17 -0700

    Validate a Rule card
    A rule card must modify a rule as well as the new value for the rule

--- lib/fluxx/card/rule.rb
+++ lib/fluxx/card/rule.rb
@@ -1,7 +1,17 @@
 require 'fluxx'
 require 'fluxx/card'
+
 class Fluxx::Card::Rule < Fluxx::Card
+  def initialize(options)
+    # Must have a modifier & the new value
+    raise Fluxx::MissingAttributeError unless options[:new] and options[:modifier]
+  end
+
   def play(ruleset, player)
     nil
+  end
+
+  def validate
+    #
   end
 end
--- test/helper.rb
+++ test/helper.rb
@@ -50,11 +50,11 @@ Fluxx::Card.create(type: :goal,
 
 Fluxx::Card.create(type: :creeper, name: "Shackles", description: "You can't win if you have this unless the Goal says otherwise.", set: :pirate)
 
-Fluxx::Card.create(type: :rule, name: 'Draw 4', description: 'Draw 4 cards', mod: :draw_limit, new: 4)
-Fluxx::Card.create(type: :rule, name: 'Draw 2', description: 'Draw 2 cards', mod: :draw_limit, new: 2)
-Fluxx::Card.create(type: :rule, name: 'Play 4', description: 'Play 4 cards', mod: :play_limit, new: 4)
-Fluxx::Card.create(type: :rule, name: 'Play 2', description: 'Play 2 cards', mod: :play_limit, new: 2)
-Fluxx::Card.create(type: :rule, name: 'Play All', description: 'Play all cards', mod: :play_limit, new: 0)
+Fluxx::Card.create(type: :rule, name: 'Draw 4', description: 'Draw 4 cards', modifier: :draw_limit, new: 4)
+Fluxx::Card.create(type: :rule, name: 'Draw 2', description: 'Draw 2 cards', modifier: :draw_limit, new: 2)
+Fluxx::Card.create(type: :rule, name: 'Play 4', description: 'Play 4 cards', modifier: :play_limit, new: 4)
+Fluxx::Card.create(type: :rule, name: 'Play 2', description: 'Play 2 cards', modifier: :play_limit, new: 2)
+Fluxx::Card.create(type: :rule, name: 'Play All', description: 'Play all cards', modifier: :play_limit, new: 0)
 
 # The most basic card, ever. No description, no set, nothing special at all.
 Fluxx::Card.create(type: :keeper, name: 'Minimal')
