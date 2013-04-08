fc60ffa4a3e375dbc88c375b9c69ce8ae975f100
Colin Shea <colin@evaryont.me>
2013-04-08 12:28:40 -0700

    Forgot to call 'super'
    The values for name, description, etc aren't set until the parent class
    initialize is called as well, which means that the name was always 'nil'

--- lib/fluxx/card/rule.rb
+++ lib/fluxx/card/rule.rb
@@ -3,6 +3,8 @@ require 'fluxx/card'
 
 class Fluxx::Card::Rule < Fluxx::Card
   def initialize(options)
+    super
+
     # Must have a modifier & the new value
     raise Fluxx::MissingAttributeError unless options[:new] and options[:modifier]
   end
